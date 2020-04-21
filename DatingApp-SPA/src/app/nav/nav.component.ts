08df39df1799d6f5d74f5f8e10fc3a8df9c23204
unknown <surajverma019@gmail.com>
2020-04-22 02:08:14 +0530

    Client Auth Service

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {
+
+  }
+  constructor(private authServices: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authServices.login(this.model).subscribe(next =>{
+      console.log('Login Successfully');
+    },error =>{
+      console.log('Failed to login');
+    });
+  }
+
+}
